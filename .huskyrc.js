a00f7e0e8ce7ed9214200b8ac51e54a4c359f18e
Ben Burke <benjamindburke@gmail.com>
2020-12-21 00:50:17 -0500

    chore: automate tests

--- .huskyrc.js
+++ .huskyrc.js
@@ -2,7 +2,7 @@ module.exports = {
   hooks: {
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
     'pre-commit': 'lint-staged',
-    'pre-push': 'run-p lint test',
+    'pre-push': 'run-p lint test:ci',
     'post-merge': 'npm i',
   },
 };
